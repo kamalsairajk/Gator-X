32790517524a3691463dce30bb303f9c047904c8
Anviksha-Sharma <56202872+Anviksha-Sharma@users.noreply.github.com>
2022-02-04 17:31:39 -0500

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+import RegisterComponent from "./RegisterComponent"
+import Login from "./Login"
+import HomePage from "./HomePage"
+import UserDetailComponent from "./UserDetailComponent"
+import Navbar from "./Navbar"
+import CreatePost from "./CreatePost"
+
+export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost}
