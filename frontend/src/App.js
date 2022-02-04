b5628aa661853216e41f63fbff15d526f84af7f1
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-02-04 09:48:19 -0500

    Remove unneccessary files and define the various components in the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, 
+Link } from "react-router-dom";
+import Layout from '/frontend/Page-elements/Layout';
+import Homepage from '/frontend/components/Homepage'
+import Review from '/frontend/components/Review'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  <Router>
+      <Routes>
+          <Route path = '/' component = {Homepage}/>
+      </Routes>
+  </Router>
+};
 
 export default App;
