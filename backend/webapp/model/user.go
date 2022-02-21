1557c2b1f6135e7ae0dcb41877baf91f7372e7ff
kamalsairajk <30821679+kamalsairajk@users.noreply.github.com>
2022-02-21 13:41:02 -0500

    user and login model

---
+++ backend/webapp/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `form:"name" json:"name" binding:"required"`
+	Email    string `form:"email" json:"email" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+	Phone    string `form:"phone" json:"phone" binding:"required"`
+}
+
+type Login struct {
+	Email    string `form:"email" json:"email" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
