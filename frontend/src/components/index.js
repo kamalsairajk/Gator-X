767f250854f4d212b2bd600b27a570720d78ecc9
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-03-02 14:59:39 -0500

    Creating a path for the explore component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,6 +3,7 @@ import Login from "./Login"
 import HomePage from "./HomePage"
 import UserDetailComponent from "./UserDetailComponent"
 import Navbar from "./Navbar"
+import Explore from "/.Explore"
 import CreatePost from "./CreatePost"
 
-export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost}
+export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost, Explore}
