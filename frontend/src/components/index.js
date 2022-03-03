c996fb18d708aecc038c5cfa95780c6afb1fe97c
Anviksha-Sharma <56202872+Anviksha-Sharma@users.noreply.github.com>
2022-03-03 11:14:06 -0500

    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,7 +3,6 @@ import Login from "./Login"
 import HomePage from "./HomePage"
 import UserDetailComponent from "./UserDetailComponent"
 import Navbar from "./Navbar"
-import Explore from "/.Explore"
 import CreatePost from "./CreatePost"
 
-export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost, Explore}
+export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost}
