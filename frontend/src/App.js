6c380beb4205110e336e0ec5dece670be7522e81
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-03-04 09:39:41 -0500

    Integrating frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,23 @@ Link } from "react-router-dom";
 import Homepage from './components/Homepage'
 import Review from './components/Review'
 
-const App = () => {
+function App(){
+  axios.defaults.baseURL = "http://localhost:9000/"
+  console.log(localStorage.getItem("user"));
+
+  return (
   <Router>
     <nav>
       <Link to = "/"> Homepage </Link>
       <Link to = "/review"> Review </Link>
     </nav>
       <Routes>
-          <Route path = '/' element = {Homepage}/>
-          <Route path = '/Review' element = {<Review />}/>
+      <Route exact path="/" render={() => <HomePage />} />
+      <Route exact path="/register" render={() => <RegisterComponent />} />
+      <Route exact path="/login" render={() => <LoginComponent />} />
       </Routes>
   </Router>
-};
+  );
+}
 
 export default App;
