2121731cc744bcd123d6d4c733cd29d22484e3b4
yuvaroshit <yuvaroshit@gmail.com>
2022-03-31 21:25:16 -0400

    frontend code refactored

--- frontend/src/components/Review.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Homepage() {
-    return <div> Add </div>;
-}
-
-export default Review;
--- frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import RegisterComponent from "./RegisterComponent"
-import Login from "./Login"
-import HomePage from "./HomePage"
-import UserDetailComponent from "./UserDetailComponent"
-import Navbar from "./Navbar"
-import CreatePost from "./CreatePost"
-
-export {RegisterComponent, Login, HomePage, UserDetailComponent, Navbar, CreatePost}
