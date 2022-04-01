2a33ab55942f0596acf3f6ac51054bbf9a0ff859
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-04-01 10:58:20 -0400

    include create urls

--- frontend/cypress/integration/Create-post.js
+++ frontend/cypress/integration/Create-post.js
@@ -3,6 +3,9 @@ describe('Post creation', function () {
     it("should create the post", function () {
         cy.visit('https://localhost:9000/')
     })
-    it("should enable the user create the post")
-    cy.get
+    
+    it("should enable the user create the post", funtion () {
+        cy.visit('https://localhost:9000/')
+        cy.contains('type').click()
+    })
 })
